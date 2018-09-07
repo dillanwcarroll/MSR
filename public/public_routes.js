838953d246c90def94213c42fbedd3c851d7f88f
Austin Gatto <austin.s.c.gatto@outlook.com>
2018-09-07 14:54:26 +1000

    aif search works

--- public/public_routes.js
+++ public/public_routes.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+
 module.exports = router
 
 //routes
 router.get('/', (req, res) => {
     res.render('index')
-})
-
-router.get('/search', (req, res) => {
-    res.render('search')
 })
 
 router.get('/about', (req, res) => {
@@ -30,4 +27,14 @@ router.get('/login', (req, res) => {
 
 router.get('/portfolio', (req, res) => {
     res.render('portfolio')
+})
+
+router.get('/search', (req, res) => {
+
+    const search = require(__dirname + '/aif-search')
+
+    var listings = search.search(req.query.term, (err,listings)=>{
+        console.log(listings)
+        res.render('search', {listings : listings})
+    })
 })
