1b075ec27edeebfb474ae002e0558e1ffb7f97b8
Aidan Thorne <thaaidstar@gmail.com>
2018-09-07 14:17:37 +1000

    Full timeline functionality and Sub header

--- public/static/scripts/map.js
+++ public/static/scripts/map.js
@@ -1,44 +1,35 @@
 var cords = [145.03411259518066, -37.82879306920354];
-var coordinates = document.getElementById('coordinates');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidHJpZ2VucGFuaW5pIiwiYSI6ImNqbDV0NXU3NjJxZTAzcnF0aWdlc3B3ZG4ifQ.emMb8nYj7XXUg70EaBVZ7g';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/trigenpanini/cjl5yll0308l82qqktmirrjcy',
+    center: mapCoords[0],
+    zoom: 3.0
+});
+
+var canvas = map.getCanvasContainer();
 
 var geojson = {
     "type": "FeatureCollection",
     "features": [
         {
             "type": "Feature",
-            "properties": {
-                "message": "Hawthorn",
-                "iconSize": [20, 20]
-            },
             "geometry": {
                 "type": "Point",
-                "coordinates": cords
+                "coordinates": mapCoords[0]
             }
-        },
+        }
     ]
 };
 
-mapboxgl.accessToken = 'pk.eyJ1IjoidHJpZ2VucGFuaW5pIiwiYSI6ImNqbDV0NXU3NjJxZTAzcnF0aWdlc3B3ZG4ifQ.emMb8nYj7XXUg70EaBVZ7g';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/trigenpanini/cjl5yll0308l82qqktmirrjcy',
-    center: cords,
-    zoom: 3.0
-});
-map.addControl(new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken
-}));
-function thingy() {
-    document.getElementById("text").innerHTML = map.getCenter();
-}
 
-// add markers to map
 geojson.features.forEach(function (marker) {
     // create a DOM element for the marker
     var el = document.createElement('div');
     el.className = 'marker';
-    el.style.width = marker.properties.iconSize[0] + 'px';
-    el.style.height = marker.properties.iconSize[1] + 'px';
+    el.style.width = '15px';
+    el.style.height = '15px';
 
     el.addEventListener('click', function () {
         window.alert(marker.properties.message);
@@ -50,11 +41,7 @@ geojson.features.forEach(function (marker) {
         .addTo(map);
 });
 
-var canvas = map.getCanvasContainer();
-
 map.on('load', function () {
-
-    // Add a single point to the map
     map.addSource('point', {
         "type": "geojson",
         "data": geojson
@@ -70,60 +57,4 @@ map.on('load', function () {
         }
     });
 
-    // When the cursor enters a feature in the point layer, prepare for dragging.
-    map.on('mouseenter', 'point', function () {
-        map.setPaintProperty('point', 'circle-color', '#d85136');
-        canvas.style.cursor = 'move';
-    });
-
-    map.on('mouseleave', 'point', function () {
-        map.setPaintProperty('point', 'circle-color', '#e24422');
-        canvas.style.cursor = '';
-    });
-
-    map.on('mousedown', 'point', function (e) {
-        // Prevent the default map drag behavior.
-        e.preventDefault();
-
-        canvas.style.cursor = 'grab';
-
-        map.on('mousemove', onMove);
-        map.once('mouseup', onUp);
-    });
-
-    map.on('touchstart', 'point', function (e) {
-        if (e.points.length !== 1) return;
-
-        // Prevent the default map drag behavior.
-        e.preventDefault();
-
-        map.on('touchmove', onMove);
-        map.once('touchend', onUp);
-    });
 });
-
-function onMove(e) {
-    var coords = e.lngLat;
-
-    // Set a UI indicator for dragging.
-    canvas.style.cursor = 'grabbing';
-
-    // Update the Point feature in `geojson` coordinates
-    // and call setData to the source layer `point` on it.
-    geojson.features[0].geometry.coordinates = [coords.lng, coords.lat];
-    map.getSource('point').setData(geojson);
-}
-
-function onUp(e) {
-    var coords = e.lngLat;
-
-    // Print the coordinates of where the point had
-    // finished being dragged to on the map.
-    coordinates.style.display = 'block';
-    coordinates.innerHTML = 'Longitude: ' + coords.lng + '<br />Latitude: ' + coords.lat;
-    canvas.style.cursor = '';
-
-    // Unbind mouse/touch events
-    map.off('mousemove', onMove);
-    map.off('touchmove', onMove);
-}
--- public/static/scripts/portfolio.js
+++ public/static/scripts/portfolio.js
@@ -3,10 +3,12 @@ var yearText = document.getElementById("textBox");
 var mapFootnoteText = document.getElementById("mapFootnote");
 var years = ['1910','1914', '1915', '1918', '1925']
 var mapFootnotes = ['Something happened','War were declared', 'call of duty: shoot a man', 'Went home', 'A humble farmer']
-var mapCoords = [[1,1],[129,90],[192.2314, -73.93754323]]
+var mapCoords = [[1,1],[129,46],[192.2314, -73.93754323], [243, 17], [40, 20]]
 
+slider.max = mapCoords.length;
 yearText.innerHTML = years[0];
 mapFootnoteText.innerHTML = mapFootnotes[0];
+
 
 // Update the current slider value (each time you drag the slider handle)
 function updateTextInput(val, max) {
@@ -14,14 +16,19 @@ function updateTextInput(val, max) {
     mapFootnoteText.innerHTML = mapFootnotes[val];
     yearText.style.marginLeft = 7.6 + ((76.8/(max -1)) * val) + "%";
     
-    if(mapCoords[val] != undefined){
+    if(val+1 <= mapCoords.length){
         map.flyTo({
             center: mapCoords[val]
         });
+        geojson.features[0].geometry.coordinates = mapCoords[val]
+        map.getSource('point').setData(geojson)
     }
     else{
         map.flyTo({
             center: [0,0]
         });
+        geojson.features[0].geometry.coordinates = mapCoords[0,0]
+        map.getSource('point').setData(geojson)
     }
+    
 }
